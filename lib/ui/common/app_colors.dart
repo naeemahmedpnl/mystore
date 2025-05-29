29ec433e87f01eaf873a2528ca9de1c5a3c205b4
Naeem Ahmed <100503509+naeemahmedpnl@users.noreply.github.com>
2025-05-29 12:43:29 +0500

    Update app_colors.dart

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -13,18 +13,15 @@ class AppColors {
   static const favoriteInactive = Colors.grey;
 }
 
-
-    
-    // Predefined appealing colors for categories with light and dark variants
     final List<List<Color>> colorPairs = [
-      [const Color(0xFF5D9CEC), const Color(0xFF4A89DC)], // Blue
-      [const Color(0xFFED5565), const Color(0xFFDA4453)], // Red
-      [const Color(0xFF48CFAD), const Color(0xFF37BC9B)], // Mint
-      [const Color(0xFFFFCE54), const Color(0xFFF6BB42)], // Yellow
-      [const Color(0xFFAC92EC), const Color(0xFF967ADC)], // Purple
-      [const Color(0xFFFC6E51), const Color(0xFFE9573F)], // Orange
-      [const Color(0xFF4FC1E9), const Color(0xFF3BAFDA)], // Sky Blue
-      [const Color(0xFFAAB2BD), const Color(0xFF656D78)], // Gray
-      [const Color(0xFFA0D468), const Color(0xFF8CC152)], // Green
-      [const Color(0xFFEC87C0), const Color(0xFFD770AD)], // Pink
+      [const Color(0xFF5D9CEC), const Color(0xFF4A89DC)], 
+      [const Color(0xFFED5565), const Color(0xFFDA4453)], 
+      [const Color(0xFF48CFAD), const Color(0xFF37BC9B)], 
+      [const Color(0xFFFFCE54), const Color(0xFFF6BB42)], 
+      [const Color(0xFFAC92EC), const Color(0xFF967ADC)], 
+      [const Color(0xFFFC6E51), const Color(0xFFE9573F)], 
+      [const Color(0xFF4FC1E9), const Color(0xFF3BAFDA)], 
+      [const Color(0xFFAAB2BD), const Color(0xFF656D78)], 
+      [const Color(0xFFA0D468), const Color(0xFF8CC152)],
+      [const Color(0xFFEC87C0), const Color(0xFFD770AD)], 
     ];
